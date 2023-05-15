628961c98baf96b0c701cbb569721208610abb1a
+ <+>
2023-05-15 16:20:25 -0400

    changes

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
 .then((resp) => resp.json())
-.then(card => console.log(card))
+.then(data => {
+    addNewCard(data);
+    console.log(data) 
+})
+
+function addNewCard(card){
+    const newCardImg = card.cards[0].image
+    const newCardImageElement = document.createElement("img")
+    newCardImageElement.src = newCardImg
+    const playerCardsElement = document.getElementById('player-cards');
+  playerCardsElement.appendChild(newCardImageElement);
+}
+
+  
+  
+  
+  
+  
+  
+   
+  
+  
+  
+  
+  
+  
+  
