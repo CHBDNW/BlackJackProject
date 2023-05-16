e80822222d9a419093004d8adad45173fd5ca9c5
James Sullivan <jsullivan8794@gmail.com>
2023-05-16 11:12:18 -0400

    3

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
-.then(resp => resp.json())
-.then(cards => {
-    const deckId = cards.deck_id
-    return deckId
-
-
-})
-
-cards(console.log(deckId))
-
-
-
-
-
-    
-
 
